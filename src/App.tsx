19332f535072c0645b022abe34d13c98584e5ae5
Nick Barnard <55743033+barnardnicholas@users.noreply.github.com>
2022-08-12 10:07:50 +0100

    Init commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./_styles/App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Ntchuva</h1>
+    </div>
+  );
+}
+
+export default App;
