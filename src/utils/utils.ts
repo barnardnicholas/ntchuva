a24066ed48d5e72d5f513121dc701411221d25c2
Nick Barnard <55743033+barnardnicholas@users.noreply.github.com>
2022-08-15 11:51:09 +0100

    Fix comments in utils

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -3,8 +3,8 @@ import { BoardColumn, BoardSquare, PathSquare, PlayerIndex } from '../types/boar
 
 /**
  * Calculate a board square's column given its index
- * @param {number} index - Index (0-31) (zero-indexed)
- * @return {number} Column number (0-7) (zero-indexed)
+ * @param i - Index (0-31) (zero-indexed)
+ * @return Column number (0-7) (zero-indexed)
  */
 export const getColumnFromIndex = (i: number): BoardColumn => {
   return (i % 8) as BoardColumn;
@@ -12,8 +12,8 @@ export const getColumnFromIndex = (i: number): BoardColumn => {
 
 /**
  * Calculate a board square's path order - 0-15 for each player in a clockwise circle
- * @param {number} index - Index (0-31) (zero-indexed)
- * @return {number} Column number (0-15) (zero-indexed)
+ * @param i - Index (0-31) (zero-indexed)
+ * @return Column number (0-15) (zero-indexed)
  */
 export const getPathOrderFromIndex = (i: number): PathSquare => {
   let intI: number = i;
@@ -25,8 +25,8 @@ export const getPathOrderFromIndex = (i: number): PathSquare => {
 
 /**
  * Build 16 squares in a circle for one player
- * @param {0 | 1} player - 0 or 1 (2 players)
- * @return {BoardSquare[]} Array of 16 square objects
+ * @param player - 0 or 1 (2 players)
+ * @return Array of 16 square objects
  */
 export const buildBoardSquares = (player: 0 | 1): BoardSquare[] => {
   return boardIndexes.map((i: number) => {
@@ -40,20 +40,41 @@ export const buildBoardSquares = (player: 0 | 1): BoardSquare[] => {
   });
 };
 
+/**
+ * Dummy function for linting
+ * @return Nothing
+ */
 export function dummyKeyDown() {
   console.log('keydown');
 }
 
+/**
+ * Given the pathSquare and player, determine of that square in in the front row
+ * @param pathSquare - the pathsquare of the square
+ * @param player - 0 or 1 (2 players)
+ * @return True if front row, false if back row
+ */
 export function isSquareInFrontRow(pathSquare: PathSquare, player: PlayerIndex) {
   if (player === 0) return pathSquare < 8;
   return pathSquare > 7;
 }
 
+/**
+ * Get the next pathSQuare in the sequence. wraps back to 0 if on last square
+ * @param pathSquare - must be between 0 and 15
+ * @return next pathsquare -  - must be between 0 and 15
+ */
 export function getNextPathSquare(pathSquare: PathSquare): PathSquare {
   if (pathSquare === 15) return 0;
   return (pathSquare + 1) as PathSquare;
 }
 
+/**
+ * Given a pathsquare and the board, get the array index
+ * @param pathSquare - must be between 0 and 15
+ * @param board - board in question
+ * @return board index
+ */
 export function getIndexOfPathSquare(pathSquare: PathSquare, board: BoardSquare[]): number {
   // given a pathSquare, get the index
   const result = board.find((square: BoardSquare) => square.pathOrder === pathSquare);
