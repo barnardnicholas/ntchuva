60c566ba51186085b87fd5bb3734d0cbee0ca24f
Nick Barnard <55743033+barnardnicholas@users.noreply.github.com>
2022-08-16 14:53:51 +0100

    Add vh style variable

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Set vh property for mobile devices
+const vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
