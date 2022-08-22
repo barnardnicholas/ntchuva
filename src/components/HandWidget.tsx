7867ddfaa96c48df73f6d907d0a916343718f982
Nick Barnard <55743033+barnardnicholas@users.noreply.github.com>
2022-08-22 15:35:52 +0100

    Tweak hand counter styles for responsiveness

--- src/components/HandWidget.tsx
+++ src/components/HandWidget.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import usePrevious from '../hooks/usePrevious';
+import useWindow from '../hooks/useWindow';
 import { PathSquare } from '../types/board';
 import { getColumnFromPathSquare } from '../utils/utils';
 import HandCounter from './HandCounter';
@@ -12,6 +13,7 @@ interface HandWidgetProps {
 }
 function HandWidget({ visible, activeSquare, hand, moveInProgress }: HandWidgetProps) {
   const prevProps = usePrevious({ moveInProgress });
+  const { vMin } = useWindow();
 
   const [extraClasses, setExtraClasses] = useState<string>('');
 
@@ -23,6 +25,7 @@ function HandWidget({ visible, activeSquare, hand, moveInProgress }: HandWidgetP
   }, [moveInProgress, prevProps.moveInProgress]);
 
   const squareWidth = 100 / 8;
+  const counterWidthPx = (vMin - 32) / 8 / 5;
 
   const defaultStyles = {
     opacity: visible ? 1 : 0,
@@ -33,9 +36,9 @@ function HandWidget({ visible, activeSquare, hand, moveInProgress }: HandWidgetP
   const styles = {
     opacity: visible ? 1 : 0,
     top: `calc(${(activeSquare > 7 ? 50 : 0) + squareWidth / 2 - 10}% )`,
-    left: `calc(${getColumnFromPathSquare(activeSquare as PathSquare) * squareWidth}% - 1rem + ${
-      squareWidth / 2
-    }%)`,
+    left: `calc(${getColumnFromPathSquare(activeSquare as PathSquare) * squareWidth}% - ${
+      squareWidth / 10
+    }% + ${squareWidth / 2}%)`,
   };
 
   const intHand: number[] = new Array(hand).fill(0).map((_: number, i: number) => i);
@@ -46,7 +49,7 @@ function HandWidget({ visible, activeSquare, hand, moveInProgress }: HandWidgetP
       style={moveInProgress ? styles : defaultStyles}
     >
       {intHand.map((n: number) => {
-        return <HandCounter key={`${n}`} top={`${0 - n * 1}rem`} />;
+        return <HandCounter key={`${n}`} top={`${0 - n * counterWidthPx - 4}px`} />;
       })}
     </div>
   );
